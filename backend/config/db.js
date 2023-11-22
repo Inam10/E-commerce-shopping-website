8ba9c01befc62cbf980b499104e7eb4eee6dc2a4
Inam10 <inamm94@gmail.com>
2023-11-23 02:14:27 +0530

    backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
-      useUnifiedTopology: true,
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
 
